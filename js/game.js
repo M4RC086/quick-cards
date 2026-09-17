377a8e592c331e6df9f2b1d389705630339a76ee
MarcoMeuli <marcomeulic@gmail.com>
2026-09-17 19:12:51 +0200

    created cards and randomness

--- js/game.js
+++ js/game.js
@@ -1,11 +1,36 @@
 const menuButton = document.getElementById('menu-button');
-let cards = JSON.parse(localStorage.getItem('cards'));
+const cardParent = document.getElementById('card-parent');
 
-console.log(cards);
+const cards = JSON.parse(localStorage.getItem('cards'));
+let notUsedCards = [...cards];
+cardParent.innerHTML = `<button class="card-button">${getNextCard()}</button>`;
 
 
 menuButton.addEventListener('click', gotoMenu);
 function gotoMenu(){
-    window.location.href = 'index.html';
-    
+  window.location.href = 'index.html';
+}
+
+
+cardParent.addEventListener('click', (e) => {
+  if (e.target.classList.contains('card-button')) {
+    clickCard();
+  }
+});
+
+function clickCard(){
+  let nextCard = getNextCard();
+  cardParent.innerHTML = `<button class="card-button">${nextCard}</button>`;
+}
+
+function getNextCard(){
+  console.log(notUsedCards);
+  if (notUsedCards.length === 0){
+    return 'No cards left';
+  }
+
+  let index = Math.floor(Math.random() * notUsedCards.length);
+  let card = notUsedCards[index];
+  notUsedCards.splice(index, 1);
+  return card;
 }
